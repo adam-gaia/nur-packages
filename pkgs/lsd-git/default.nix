855f94fa5edf2e562d83f38f142f34edf22333eb
Adam Gaia <git@agaia.dev>
2023-08-15 06:42:21 -0600

    fix: fix shinydir packaging

--- pkgs/lsd-git/default.nix
+++ pkgs/lsd-git/default.nix
@@ -1,19 +1,37 @@
-{ lib, fetchFromGitHub, rustPlatform }:
+{ lib, fetchgit, testers, rustPlatform, installShellFiles, pandoc, lsd }:
 
+# Modified from the offical nixpkg https://github.com/NixOS/nixpkgs/blob/9c85697da8e59561151aa279df44ae51a367ed7d/pkgs/tools/misc/lsd/default.nix
 rustPlatform.buildRustPackage rec {
   pname = "lsd";
-  version = "0.23.1";
+  version = "master";
 
-  src = fetchFromGitHub {
-  owner = "lsd-rs";
-  repo = "lsd";
-  rev = "8acaabec723cdd71b498212c8a47ac952d022738";
-  hash = "sha256-3qsJrHFmMn7TWFjTx7C6SdPKu1sxzZPopzXYG7s7Kok=";
-};
+  src = fetchgit {
+    url = "https://github.com/lsd-rs/lsd.git";
+    rev = version;
+    hash = "";
+  };
 
-  cargoHash = "sha256-cCqMxLKEbiTEQ1ZjnIurre3axdzHMKiygDRd6o12aYs=";
+  cargoSha256 = "sha256-t7J7hIbLlRq99Yd2/3Zn+PbHhJtaJRdDluDXN0Hp/Jc=";
+
+  nativeBuildInputs = [ installShellFiles pandoc ];
+  postInstall = ''
+    pandoc --standalone --to man doc/lsd.md -o lsd.1
+    installManPage lsd.1
+
+    installShellCompletion $releaseDir/build/lsd-*/out/{_lsd,lsd.{bash,fish}}
+  '';
+
+  # Found argument '--test-threads' which wasn't expected, or isn't valid in this context
+  doCheck = false;
+
+  passthru.tests.version = testers.testVersion {
+    package = lsd;
+  };
 
   meta = with lib; {
-    platforms = platforms.all;
+    homepage = "https://github.com/Peltoche/lsd";
+    description = "The next gen ls command";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ Br1ght0ne marsam zowoq SuperSandro2000 ];
   };
 }
--- pkgs/shinydir/default.nix
+++ pkgs/shinydir/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "shinydir";
-  version = "0.1.2";
+  version = "v0.1.2";
 
   src = fetchFromGitHub {
     owner = "Unoqwy";
     repo = pname;
     rev = version;
-    hash = "sha256-Mr7JNt8bFZRqSOTC4U8iOxzCL/+RIhwAMfLsOAkouY0=";
+    hash = "sha256-n6yJ9iE9lThkRkMjyIzGZ+Hi/sS6tYFfngC7sYiJHIw=";
   };
 
-  cargoHash = "sha256-uhIdVnpV3GY4rQLZYKGGpxpQNsF7FRP4jFoBPgvUsd8=";
+  cargoHash = "sha256-71cDG7Jplny9RI3glIH9qBtlGYDn4C4tkGCHUmlvkjk=";
 
   meta = with lib; {
     platforms = platforms.all;
