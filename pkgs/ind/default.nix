918685880fe8106e3869a9fca35b4afa085d8d6d
Adam Gaia <git@agaia.dev>
2023-08-21 15:29:06 -0600

    feat: add ind

--- pkgs/ind/default.nix
+++ pkgs/ind/default.nix
@@ -1,11 +1,29 @@
-{ lib, fetchFromGitHub }:
-let 
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "ind";
+  version = "0.2.2";
+
+  # Enable nightly features somehow
+  # https://nixos.org/manual/nixpkgs/stable/#using-community-maintained-rust-toolchains
+  RUSTC_BOOTSTRAP = 1;
+ 
   src = fetchFromGitHub {
     owner = "adam-gaia";
     repo = "ind";
-    rev = "cf62f87ec0dd0cf232bbc25de796ced6bb6b478b";
-    sha256 = "sha256-3RKeNHubaL9Le3K0cHkIb5n9AXGjLmISbui4ovCJbqg=";
+    rev = "bc38035da367eadd8a784064915eb7da85b6d16b";
+    hash = "sha256-0uCAWojL/Atrjihat1wiTVak29SuzzMAcZr8K/mtKPQ=";
   };
-in
-  import "${src}/flake.nix"
 
+  cargoHash = "sha256-nQa8yrXPXI4D3O8AkYgWucTRYgKZohIS5VQtsolUCQA="; 
+
+  meta = with lib; {
+    description = "TODO";
+    homepage = "TODO";
+    license = with licenses; [ mit ];
+    maintainers = [ "Adam Gaia" ];
+  };
+}
